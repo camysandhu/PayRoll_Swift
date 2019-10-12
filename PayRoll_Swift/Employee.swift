48c99dd7fc1b995af382edbe178ff8c4bda90cd8
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-11 21:27:44 -0400

    employee

---
+++ PayRoll_Swift/Employee.swift
@@ -0,0 +1,20 @@
+//
+//  Employee.swift
+//  PayRoll_Swift
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Employee{
+    print("Class employee is created")
+}
+
+
+
+
+
+
+
+
