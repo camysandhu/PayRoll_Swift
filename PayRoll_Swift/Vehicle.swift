f5ce5512e41b6ac056042bb2ccbfb1cc726ffbff
simarmahi <Simarmahi7696>
2019-10-13 12:58:15 -0400

    Vehicle Class\Declaring variables

--- PayRoll_Swift/Vehicle.swift
+++ PayRoll_Swift/Vehicle.swift
@@ -1,1 +1,10 @@
-print("Starting project---SWIFT")
+class Vehicle
+{
+    private var make: String
+    private var color: String
+    private var plate: String
+    
+    init() {
+        
+    }
+}
